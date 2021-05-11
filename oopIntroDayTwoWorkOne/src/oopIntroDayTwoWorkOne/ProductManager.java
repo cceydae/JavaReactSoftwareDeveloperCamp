e5dbf15a60ebcaee78e5bb832bf92a349fc398c7
Ceyda Eser <ceydae.1040@gmail.com>
2021-05-11 14:24:58 +0300

    day2 work1
    Java oop ıntro

---
+++ oopIntroDayTwoWorkOne/src/oopIntroDayTwoWorkOne/ProductManager.java
@@ -0,0 +1,11 @@
+package oopIntroDayTwoWorkOne;
+
+public class ProductManager {
+	public void startCourse(Product product) {
+		System.out.println("Kursa baþladýnýz: "+ product.name);
+	}
+	
+	public void nextLesson() {
+		System.out.println("Sonraki derse geçildi");
+	}
+}
